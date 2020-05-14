63e4610a02c6de323facffef324e2a1f755fcac9
javierfurus <javierfurus@gmail.com>
2020-05-14 19:30:52 +0200

    Forgot to add calculator.js

--- calculator.js
+++ calculator.js
@@ -1,10 +1,11 @@
 const div = (a, b) => { // Ez itt most egy semistandard tűrő comment
-	return a / b; // itt van megint egy
-}
+  return a / b; // itt van megint egy
+};
 const sum = (a, b) => {
-	return a + b;
-}
+  return a + b;
+};
 
 module.exports = {
-sum: sum
-}
+  sum: sum,
+  div: div
+};

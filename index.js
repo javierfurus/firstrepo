fbe86879361d30b6a377d8aae345d2e213eb4d67
javierfurus <javierfurus@gmail.com>
2020-05-14 19:28:00 +0200

    First commit on the development branch

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-//egy ujabb komment sor
+// egy ujabb komment sor
 // pelda komment sor
+// ez egy blalba
 const calculator = require('./calculator');
 
-console.log(calculator.sum(10,12));
-
+console.log(calculator.sum(10, 12));
 
 console.log(calculator.div(12, 4));
 
 console.log('Hello FlowWork!');
-
